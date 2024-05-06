4b0232aa3965f61f3b5d774b323035d3fc09e0c7
fenia3004 <fenia3004@gmail.com>
2024-05-06 11:35:37 +0300

    Вторая миграци "добавление поле «Дата производства продукта» (manufactured_at)"" (ТЕСТ)

--- config/settings.py
+++ config/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_homework',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '1111',
         'HOST': 'localhost',
-        'PORT': '5432'
+        'PORT': '5433'
     }
 }
 
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR/'static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
